39bde161aa914fdf8a783c25295ab6086e7968d3
ZhaoYideng <zhaoyideng@hotmail.com>
2017-09-20 01:06:57 +0800

    test

---
+++ fyp/package_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+find . -name '__pycache__' -type d -exec rm -r {} +
+find . -name '.cache' -type d -exec rm -r {} +
+find . -name '.idea' -type d -exec rm -r {} +
+find . -name '.DS_Store' -exec rm -f {} +
+find . -name '*.pyc' -exec rm -f {} +
+find . -name 'report.html' -exec rm -f {} +
+find . -name 'assets' -type d -exec rm -r {} +
+
+pip freeze > requirements.txt
--- fyp/run_tests.py
+++ fyp/run_tests.py
@@ -7,4 +7,4 @@ import pytest
 
 path = os.getcwd()
 test_script_path = os.path.join(path, 'tests', 'tests', 'demo_test.py')
-pytest.main(['-x', test_script_path, '--html=test_report.html'])
+pytest.main(['-x', test_script_path, '--html=report.html'])
