5acd8b0829cc74aa62c490bb78cf3d16d57bb2f8
ZhaoYideng <yideng.zhao@viki.com>
2018-03-24 11:54:51 +0800

    2

--- fyp/tests/utils/appium_wrapper.py
+++ fyp/tests/utils/appium_wrapper.py
@@ -4,18 +4,39 @@
 from time import sleep
 import subprocess
 import os
+from fancy_log import fancy_log
 
 
-def startAppium(port=4723):
+def startAppium(port):
     stopAppium(port)
-    print 'appium_py::utils::appium_wrapper::start_appium(%s)' % port
-    start_appium = 'nohup appium -p %s --log-level error &' % port
+    fancy_log(port=port)
+    start_appium = 'nohup appium -p %s > nohup-%s.out &' % (port, port)
+    max_retry = 100
+    retry = 0
+    f = None
+    server_log = os.path.abspath('./nohup-%s.out' % port)
+    fancy_log(logfile=server_log)
     subprocess.Popen(start_appium, shell=True)
-    sleep(15)
+    while retry < max_retry:
+        try:
+            f = open(server_log, 'r')
+            if f.read():
+                print 'Appium Server started !'
+                break
+            else:
+                print 'Appium Server Initialing...'
+        except IOError:
+            print 'Waiting for Appium Server...'
+        finally:
+            retry += 1
+            sleep(3)
+            if f:
+                f.close()
+    sleep(3)
 
 
-def stopAppium(port=4723, timeout=5):
-    print 'appium_py::utils::appium_wrapper::stop_appium(%s)' % port
+def stopAppium(port, timeout=5):
+    fancy_log(port=port)
     kill_appium = "ps axu | grep appium | grep -v grep | grep %s | awk '{print $2}' | xargs kill -9 " % port
     os.popen(kill_appium)
     sleep(timeout)
