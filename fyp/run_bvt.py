9989c9b1e195e462f1664e1c664958b0ed16878f
ZhaoYideng <yideng.zhao@viki.com>
2018-03-25 21:21:06 +0800

    last modify

--- fyp/run_bvt.py
+++ fyp/run_bvt.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 
-import subprocess
-import re
 import pytest
 import os
-import sys
-from shutil import copyfile, copytree
 from BeautifulSoup import BeautifulSoup
 
 """start tests"""
 os.environ['root_dir'] = os.getcwd()
-copyfile(os.environ['apk'], os.environ['root_dir'])
 os.environ['app'] = os.path.join(os.environ['root_dir'], 'android-debug.apk')
 test_script_path = os.path.join(os.environ['root_dir'], 'tests', 'tests')
 pytest.main(['-s', test_script_path, '--html=bvt_report.html'])
@@ -24,9 +19,3 @@ for each in result:
     passed += each.tr.contents[3].text + '\n'
 with open("result.txt", "w") as f:
     f.write(passed)
-
-copyfile('bvt_report.html', '/Applications/XAMPP/htdocs/temp/bvt_report.html')
-copyfile('security_test_report.html', '/Applications/XAMPP/htdocs/temp/security_test_report.html')
-copyfile('monkey_test_report.html', '/Applications/XAMPP/htdocs/temp/monkey_test_report.html')
-copyfile('result.txt', '/Applications/XAMPP/htdocs/temp/result.txt')
-copytree('screenshots', '/Applications/XAMPP/htdocs/temp/screenshots')
