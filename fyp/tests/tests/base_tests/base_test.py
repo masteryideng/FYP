dc688df54c891f40c49afaa92bb5f28a77c69b7c
masteryideng <zhaoyideng@hotmail.com>
2018-04-15 21:45:50 +0800

    Before Demo

--- fyp/tests/tests/base_tests/base_test.py
+++ fyp/tests/tests/base_tests/base_test.py
@@ -18,26 +18,15 @@ PATH = lambda p: os.path.abspath(
 class BaseTest(unittest.TestCase):
     """Basis for all tests."""
 
-    # appium
-    MAX_RETRY = 100
-    APPIUM_MIN_PORT = 4000
-    APPIUM_MAX_PORT = 5000
-    # WDA
-    WDA_MIN_LOCAL_PORT = 8000
-    WDA_MAX_LOCAL_PORT = 9000
+    port = 4723
 
     @classmethod
     def setUpClass(cls):
-        port = random.randint(cls.APPIUM_MIN_PORT, cls.APPIUM_MAX_PORT)
-        server_log = os.path.abspath('./nohup-%s.out' % port)
-        if os.path.isfile(server_log):
-            os.remove(server_log)
-        cls.appium_port = port
-        startAppium(cls.appium_port)
+        startAppium(cls.port)
 
     @classmethod
     def tearDownClass(cls):
-        stopAppium(cls.appium_port)
+        stopAppium(cls.port)
 
     def setUp(self):
         desired_caps = {}
@@ -46,8 +35,7 @@ class BaseTest(unittest.TestCase):
         desired_caps['deviceName'] = os.environ['deviceName']
         desired_caps['app'] = os.environ['app']
 
-        url = 'http://0.0.0.0:%s/wd/hub' % self.appium_port
-        self.driver = webdriver.Remote(url, desired_caps)
+        self.driver = webdriver.Remote('http://localhost:%s/wd/hub' % self.port, desired_caps)
 
         try:
             self.driver.switch_to.alert.accept()
@@ -57,22 +45,3 @@ class BaseTest(unittest.TestCase):
     def tearDown(self):
         """Shuts down the driver."""
         self.driver.quit()
-
-    '''
-    port = 4723
-
-    def setUp(self):
-
-        startAppium(self.port)
-        desired_caps = {}
-        desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = os.environ['platformVersion']
-        desired_caps['deviceName'] = os.environ['deviceName']
-        desired_caps['app'] = os.environ['app']
-        self.driver = webdriver.Remote('http://localhost:%s/wd/hub' % self.port, desired_caps)
-        sleep(10)
-
-    def tearDown(self):
-        """Shuts down the driver."""
-        self.driver.quit()
-        stopAppium(self.port)'''
--- fyp/tests/utils/appium_wrapper.py
+++ fyp/tests/utils/appium_wrapper.py
@@ -4,39 +4,15 @@
 from time import sleep
 import subprocess
 import os
-from fancy_log import fancy_log
 
 
-def startAppium(port):
+def startAppium(port=4723):
     stopAppium(port)
-    fancy_log(port=port)
-    start_appium = 'nohup appium -p %s > nohup-%s.out &' % (port, port)
-    max_retry = 100
-    retry = 0
-    f = None
-    server_log = os.path.abspath('./nohup-%s.out' % port)
-    fancy_log(logfile=server_log)
-    subprocess.Popen(start_appium, shell=True)
-    while retry < max_retry:
-        try:
-            f = open(server_log, 'r')
-            if f.read():
-                print 'Appium Server started !'
-                break
-            else:
-                print 'Appium Server Initialing...'
-        except IOError:
-            print 'Waiting for Appium Server...'
-        finally:
-            retry += 1
-            sleep(3)
-            if f:
-                f.close()
-    sleep(3)
+    os.system('appium --port %s &' % port)
+    sleep(20)
 
 
-def stopAppium(port, timeout=5):
-    fancy_log(port=port)
+def stopAppium(port=4723, timeout=5):
     kill_appium = "ps axu | grep appium | grep -v grep | grep %s | awk '{print $2}' | xargs kill -9 " % port
     os.popen(kill_appium)
     sleep(timeout)
